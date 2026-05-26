85f4505c1a8ed9fe20489c549ff129f43dbb5e7b
MysticMet2049 <metapinguediayannulrich@gmail.com>
2026-05-26 08:21:27 +0100

    tailwind was added

---
+++ Frontend/Equator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <div className="App">Equator</div>
+    </>
+  )
+}
+
+export default App
